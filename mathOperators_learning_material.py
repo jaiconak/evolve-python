579ae2a92a7d3f272c75ddb919221073e630d9f1
Jaico Seth Nakpil <jaicosethnakpil@ForeignJayMachine.local>
2025-01-02 11:18:49 -0500

    lectures

---
+++ mathOperators_learning_material.py
@@ -0,0 +1,22 @@
+# Create a variable and assign it the sum of two integers
+# Create a variable and assign it the difference of two integers
+# Create a variable and assign it the result of one integer being divided by another integer
+# Create a variable and assign it the product of two integers
+# Create a variable and assign it the result of 3 raised to the 8th power
+# Create a variable and assign it the result of 10 floor division 3
+# Create a variable and assign it the integer 2 using the expression 17 modulo [number]
+
+num1 = 20
+num2 = 7
+
+sum = num1 + num2 #prob1
+difference = num1 - num2 #prob2
+divide = num1/num2 #prob3
+product = num1 * num2 #prob4
+exponents = 3 **8 #prob 5
+floor = 10 // 3
+modulo = 17 % 5
+
+
+
+print (sum, difference, divide, product, exponents, floor, modulo)
---
+++ variable_learning_material.py
@@ -0,0 +1,11 @@
+# 1. Create a variable and assign it an integer
+# 2. Create a second variable and assign it a float
+# 3. Create a third variable and assign it a Boolean Value
+# 4. Re-assign a different integer to the first varibale you created 
+
+intX = 27
+intY = 27.20
+intZ = True
+intX = 28
+
+print (intX,intY,intZ)
