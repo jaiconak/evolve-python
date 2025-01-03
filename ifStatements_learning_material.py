15926e94676f6981dd193fd2e7779c2425a8261e
Jaico Seth Nakpil <jaiconak@gmail.com>
2025-01-03 13:11:33 -0500

    loop learning

--- ifStatements_learning_material.py
+++ ifStatements_learning_material.py
@@ -1,4 +1,5 @@
-studentScore = int (input("What is your score? "))
+from random import randint 
+# studentScore = int (input("What is your score? "))
 
 # gradeA = studentScore >=90
 # gradeB = studentScore >=80
@@ -6,17 +7,52 @@ studentScore = int (input("What is your score? "))
 # gradeD = studentScore >=60
 # gradeF = studentScore >=59
 
-if (studentScore >=90) :
-    print ("You have recieved an A grade!")
-    
-if studentScore >=80 and studentScore < 90:
-    print ("You have recieved an B grade!")
+# if studentScore >= 90 :
+#     print ("You will recieve an A grade")
+# else:
+#     if studentScore >=80 :
+#         print ("You will receive an B grade")
+#     else:
+#         if studentScore >=70 :
+#             print ("You will receieve an C grade")
+#         else:
+#             if studentScore >=60 :
+#                 print ("You will receive an D grade")
+#             else:
+#                 print ("You have failed this class.. F!")
 
-if (studentScore >=70 and studentScore <80) :
-    print ("You have recieved an C grade!")
+# Now will do it in elif statements
 
-if (studentScore >=60 and studentScore <70) :
-    print ("You have recieved an D grade!")
+# if studentScore >= 90 :
+#     print("You will receive an A grade")
+# elif studentScore >=80 :
+#     print("You will receive an B grade")
+# elif studentScore >=70 :
+#     print ("You will recieve an C grade")
+# elif studentScore >=60 :
+#     print("You will receive an D grade")
+# else :
+#     print ("You failed bud")
 
-if (studentScore <60) :
-    print ("You have failed this class... GRADE")
+randomNum = randint (1,10)
+print(randomNum, "is the randomized number and in roman numerals its displayed as: ")
+if randomNum == 1 :
+    print ("I")
+elif randomNum == 2 :
+    print ("II")
+elif randomNum == 3 :
+    print ("III")
+elif randomNum == 4 :
+    print ("IV")
+elif randomNum == 5 :
+    print ("V")
+elif randomNum == 6 :
+    print ("VI")
+elif randomNum == 7 :
+    print ("VII")
+elif randomNum == 8 :
+    print ("VIII")
+elif randomNum == 9 :
+    print ("IX")
+elif randomNum == 10 :
+    print ("X")
