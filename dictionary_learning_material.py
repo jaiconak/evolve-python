366d1d3baa021d9e9085e2d53a7c4cdfd6e15f46
Jaico Seth Nakpil <jaiconak@gmail.com>
2025-01-06 11:31:54 -0500

    dictionary and list

---
+++ dictionary_learning_material.py
@@ -0,0 +1,9 @@
+car = {
+    "make" : "BMW",
+    "year" : 2021,
+    "engine" : "v6"
+}
+
+car["color"] = "Black"
+# del car["color"]
+print (car)
---
+++ list_learning_module.py
@@ -0,0 +1,6 @@
+favFruits = ["Pineapple","Mango","Cherries"]
+print(favFruits)
+favFruits.append("Apple")
+print(favFruits)
+for i in range(len(favFruits)) :
+    print (favFruits[i])
